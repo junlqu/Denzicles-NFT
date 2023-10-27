b39aa022aa53794c8016cf92abd14a97d26575a1
junlqu <17263194+junlqu@users.noreply.github.com>
2023-10-27 14:40:47 -0400

    Added transfer functions

--- src/contracts/Denzicle.sol
+++ src/contracts/Denzicle.sol
@@ -3,21 +3,10 @@ pragma solidity ^0.8.20;
 
 import "./Denzicle721.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 interface Denz {
-  /**
-   * Burns a selected amount of tokens from a wallet address
-   * @param _from the address to burn tokens from
-   * @param amount the amount of tokens to burn
-   */
   function burn(address _from, uint256 amount) external;
-
-  /**
-   * Updates the rewards claiming list with the total claimable amount of tokens from one address to another
-   * This is used when a Denzicle is transferred to another wallet
-   * @param _from the original wallet of the Denzicle
-   * @param _to the new wallet of the Denzicle
-   */
   function updateClaim(address _from, address _to) external;
 }
 
@@ -30,22 +19,81 @@ contract Denzicle is Initializable, Denzicle721 {
   struct DenzicleData {
     string name;
     string features;
+    uint256 rangeLevel;
+    uint256 damageLevel;
+    uint256 speedLevel;
+    uint256 cost;
+    uint256 lives;
   }
 
-  modifier denzicleOwner(uint256 denzicleId) {
-    require(ownerOf(denzicleId) == msg.sender, "You do not own this Denzicle!");
-    _;
+  struct DenzermData {
+    string name;
+    string features;
   }
 
   Denz public denzCoin;
 
   mapping(uint256 => DenzicleData) public denzicleData;
 
-  function initialize(uint256 _maxSupply, uint256 _price, string memory _uri) public initializer {
+  /**
+   * @notice checks if the wallet owns a selected Denzicle
+   */
+  modifier denzicleOwner(uint256 denzicleId) {
+    require(ownerOf(denzicleId) == msg.sender, "You do not own this Denzicle!");
+    _;
+  }
+
+  /**
+   * Constructor function
+   * @param _maxSupply the maximum original denzicles
+   * @param _maxCount the maximum count of all denzicle and denzerms
+   * @param _price the price of minting denzicles
+   * @param _uri the base URI
+   */
+  function initialize(uint256 _maxSupply, uint256 _maxCount, uint256 _price, string memory _uri) public initializer {
     __ERC721_init_unchained(_name, _symbol);
     __Ownable_init_unchained(msg.sender);
     maxSupply = _maxSupply;
+    maxCount = _maxCount;
     price = _price;
     baseURI = _uri;
+  }
+
+  /**
+   * Sets the address of the DenzCoin contract
+   * @param _denzCoinAddress the DenzCoin address
+   */
+  function setDenzCoin(address _denzCoinAddress) external onlyOwner{
+    denzCoin = Denz(_denzCoinAddress);
+  }
+
+  /**
+   * Updates the transferFrom function to update local values as well
+   * @param from the from address
+   * @param to the too address
+   * @param tokenId the Denzicle transfered
+   */
+  function transferFrom(address from, address to, uint256 tokenId) public override(ERC721Upgradeable, IERC721) {
+    if (tokenId < maxCount) {
+      denzCoin.updateClaim(from, to);
+      balanceDenzicle[from]--;
+      balanceDenzicle[to]++;
+    }
+    ERC721Upgradeable.transferFrom(from, to, tokenId);
+  }
+
+  /**
+   * Updates the safeTransferFrom function to update local values as well
+   * @param from the from address
+   * @param to the too address
+   * @param tokenId the Denzicle transfered
+   */
+  function safeTransferFrom(address from, address to, uint256 tokenId, bytes memory data) public override(ERC721Upgradeable, IERC721) {
+    if (tokenId < maxCount) {
+      denzCoin.updateClaim(from, to);
+      balanceDenzicle[from]--;
+      balanceDenzicle[to]++;
+    }
+    ERC721Upgradeable.safeTransferFrom(from, to, tokenId, data);
   }
 }
